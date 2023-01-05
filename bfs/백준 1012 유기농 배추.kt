11ed11374695113dae227b7283b0363865a978d1
최지환 <54761791+Choe-Ji-Hwan@users.noreply.github.com>
2023-01-05 11:53:00 +0900

    Update 백준 1012 유기농 배추.kt

--- bfs/백준 1012 유기농 배추.kt
+++ bfs/백준 1012 유기농 배추.kt
@@ -1,14 +1,12 @@
 // 백준 1012번 유기농 배추
 // 배추가 상하좌우 네 방향
-// Silver2
+// 
 
 import java.util.LinkedList
 import java.util.StringTokenizer
 
 fun main() = with(System.`in`.bufferedReader()) {
     val t = readLine().toInt()  // 테스트 케이스
-    val dy = listOf(-1, 1, 0, 0)
-    val dx = listOf(0, 0, -1, 1)
     val sb = StringBuilder()
 
     repeat(t) {
@@ -28,32 +26,39 @@ fun main() = with(System.`in`.bufferedReader()) {
             map[y][x] = true
         }
 
-        var count = 0
+        val count = bfsCount(map, visited, m, n)
+        sb.append("$count\n")
+    }
+    print(sb)
+}
 
-        for (x in 0 until m) {
-            for (y in 0 until n) {
-                if (map[y][x] && visited[y][x].not()) {
-                    val queue = LinkedList<Pair<Int, Int>>()    // 왜 ArrayDeque 보다 LinkedList를 사용하는 것이 더 빠를까?를 알아야한다.
-                    queue.add(Pair(y, x))
-                    while (queue.isEmpty().not()) {
-                        val (col, row) = queue.poll()
-                        map[col][row] = false
-                        for (i in 0 until 4) {
-                            val newCol = col + dy[i]
-                            val newRow = row + dx[i]
-                            if (newCol !in 0 until n || newRow !in 0 until m) continue
-                            if (map[newCol][newRow].not()) continue
-                            if (visited[newCol][newRow]) continue
+fun bfsCount(map: Array<BooleanArray>, visited: Array<BooleanArray>, m: Int, n: Int): Int {
+    val dy = listOf(-1, 1, 0, 0)
+    val dx = listOf(0, 0, -1, 1)
+    var count = 0
 
+    for (x in 0 until m) {
+        for (y in 0 until n) {
+            if (map[y][x] && visited[y][x].not()) {
+                val queue = LinkedList<Pair<Int, Int>>()
+                queue.add(Pair(y, x))
+                while (queue.isEmpty().not()) {
+                    val (col, row) = queue.poll()
+                    map[col][row] = false
+                    for (i in 0 until 4) {
+                        val newCol = col + dy[i]
+                        val newRow = row + dx[i]
+                        if (newCol in 0 until n && newRow in 0 until m &&
+                            map[newCol][newRow] && visited[newCol][newRow].not()
+                        ) {
                             queue.add(Pair(newCol, newRow))
                             visited[newCol][newRow] = true
                         }
                     }
-                    count++
                 }
+                count++
             }
         }
-        sb.append("$count\n")
     }
-    print(sb)
+    return count
 }
