30a1ccbbd21535efaf6993251de28163c55880ed
최지환 <54761791+Choe-Ji-Hwan@users.noreply.github.com>
2024-08-15 13:48:36 +0900

    Update 백준 9251 LCS.py

--- dp/백준 9251 LCS.py
+++ dp/백준 9251 LCS.py
@@ -6,14 +6,15 @@ input = sys.stdin.readline
 
 first = list(input().rstrip())
 second = list(input().rstrip())
+max_size = max(len(first), len(second))
 
-matrix = [[0 for _ in range(len(first) + 1)] for _ in range(len(second) + 1)]
+matrix = [[0 for _ in range(max_size + 1)] for _ in range(max_size + 1)]
 
-for y in range(1, len(second) + 1):
-    for x in range(1, len(first) + 1):
-        if second[y - 1] == first[x - 1]:
-            matrix[y][x] = matrix[y - 1][x - 1] + 1
+for i in range(1, len(first) + 1):
+    for j in range(1, len(second) + 1):
+        if first[i - 1] == second[j - 1]:
+            matrix[i][j] = matrix[i - 1][j - 1] + 1
         else:
-            matrix[y][x] = max(matrix[y - 1][x], matrix[y][x - 1])
+            matrix[i][j] = max(matrix[i - 1][j], matrix[i][j - 1])
 
-print(matrix[-1][-1], end="")
+print(matrix[len(first)][len(second)])
